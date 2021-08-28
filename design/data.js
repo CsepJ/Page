9dc38a4f9932547aa80292c91681264c84f73b4b
CsepJ <sepbot3240@gmail.com>
2021-08-28 15:19:35 +0900

    Renewal Design

---
+++ design/data.js
@@ -0,0 +1,9 @@
+let text = {
+    notice: ["셉봇RPG(SPG)를 만들고 있는 중입니다(약 1-2개월)","소네트봇은 곧 삭제될 예정입니다"],
+};
+const url = {
+    cmd: "https://sepbot.repl.co/cmd",
+    update: "https://sepbot.repl.co/update",
+    support: "https://discord.gg/rJsuRUajSu",
+    dev: "https://github.com/CsepJ"
+}
