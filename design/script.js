17ca2605d9a6d428d7a9c7f09e9695fd1b18238a
Sepbot_ <68334508+CsepJ@users.noreply.github.com>
2021-08-28 15:24:21 +0900

    Revert "Renewal Design"

--- design/script.js
+++ design/script.js
@@ -1,40 +1,120 @@
-document.addEventListener("DOMContentLoaded", async event => {
-    const logo = $("#logo"), invite = $("#invite"), notice = $("#notice"), commands = $("#commands"), update = $("#update"), support = $("#support"), server = $("#server"), page = $("#page");
+const text = {
+    notice: "Nyunbot: 서비스 종료<br>Sonnet(음악봇): 출시",
+    update: "Sepbot:노래기능 삭제<br>Sepbot:퀴즈기능 추가"
+};
+const url = {
+    cmd: "https://Sepbot.repl.co/cmd",
+    support: "https://discord.gg/rJsuRUajSu",
+    dev: "https://github.com/CsepJ"
+}
+const msg = {
+    update: `<p id="title" style="color:snow">업데이트</p><p style="color: snow">${text.update}</p>`,
+    notice: `<p id="title" style="color:snow">공지사항</p><p style="color: snow;">${text.notice}</p>`,
+    support: '\<p><a href="https://discord.gg/kVMCumDa" id="discord" style="text-decoration:none; cursor:default; color:skyblue;">디스코드</a></p>',
+    welcome: "<p>SepCod</p>"
+};
+document.addEventListener("DOMContentLoaded", event => {
+    const trigger = $('#trigger');
+    const menu = $('nav ul');
+    const logo = $('#logo');
+    const bot = $('#bot');
+    const support = $("#support_link");
+    const command = $("#command");
+    const notice = $("#notice");
+    const update = $("#update");
+    const dev = $("#SepJ");
+    const menu_slide = function(){
+        var width = $(window).width();
+        if(width<=720){
+            menu.slideToggle();
+            return true;
+        }else{
+            return false;
+        }
+    }
+    // Hover Event
+    dev.hover(() => {
+        dev.css("color", "blue");
+        dev.css("text-decoration", "underline");
+    }, () => {
+        dev.css("color", "#0ff193");
+        dev.css("text-decoration", "none");
+    });
+    support.hover(() => {
+        support.css("color", "blue");
+        support.css("text-decoration", "underline");
+    }, () => {
+        support.css("color", "#0ff193");
+        support.css("text-decoration", "none");
+    });
+    logo.hover(() => {
+        logo.css("color", "#d144f4");
+    },() => {
+        logo.css("color", "#0ff193");
+    });
+    bot.hover(() => {
+        bot.css("color", "#0ff193");
+    },() => {
+        bot.css("color", "rgb(230, 220, 130)");
+    });
+    command.hover(() => {
+        command.css("color", "#0ff193");
+    },() => {
+        command.css("color", "rgb(230, 220, 130)");
+    });
+    notice.hover(() => {
+        notice.css("color", "#0ff193");
+    },() => {
+        notice.css("color", "rgb(230, 220, 130)");
+    });
+    update.hover(() => {
+        update.css("color", "#0ff193");
+    },() => {
+        update.css("color", "rgb(230, 220, 130)");
+    });
+    // Click Event
+    $(dev).on("click", ()=>{
+        window.open(url.dev);
+        menu_slide();
+    });
+    $(support).on("click", () => {
+        window.open(url.support);
+        menu_slide();
+    })
+    $(update).on("click", () => {
+        document.getElementById("page").innerHTML = `${msg.update}`;
+        menu_slide();
+    });
     $(logo).on("click", () => {
-        page.html(``);
+        document.getElementById("page").innerHTML = "";
+        menu_slide();
     });
-    $(notice).on("click", () => {
-        page.html(text.notice.join("<br>".repeat(3)));
+    $(bot).on("click", () => {
+        window.open("invite.html");
+        menu_slide();
     });
-    $(commands).on("click", () => {
+    $(command).on("click", () => {
         const xhr = new XMLHttpRequest();
         xhr.open("POST", url.cmd);
         xhr.send(null);
         xhr.onload = function(){
             const result = JSON.parse(xhr.response).map((cmd)=>cmd.cmd+" | "+cmd.description);
-            page.html("<div style='text-align:center; padding-bottom:30%;'>"+result.join("<br><br>")+"</div>");
+            $("#page").html("<div style='text-align:center; padding-bottom:30%;'>"+result.join("<br><br>")+"</div>");
+            menu_slide();
         }
     });
-    $(update).on("click", () => {
-        const xhr = new XMLHttpRequest();
-        xhr.open("POST", url.update);
-        xhr.send(null);
-        xhr.onload = function(){
-            const result = JSON.parse(xhr.responseText);
-            page.html("<div>"+result.version+"버전<br>"+result.log+"</div>");
+    $(notice).on("click", () => {
+        document.getElementById("page").innerHTML = `${msg.notice}`
+        menu_slide();
+    });
+    $(trigger).on('click', function(e) {
+        e.preventDefault();
+        menu.slideToggle()
+    });
+    $(window).resize(function() {
+        var w = $(window).width();
+        if (w > 320 && menu.is(':hidden')) {
+            menu.removeAttr('style')
         }
-    });
-    $(invite).on("click", () => {
-        page.html(`
-        <div>
-        <a href="https://sepcod.com/sepbot" alt="Sepbot" title="Sepbot">셉봇(Sepbot) 초대</a>
-        <br>
-        <a style="text-decoration: line-through;" href="https://sepcod.com/sonnet" alt="Sonnet" title="Sonnet">소네트(Sonnet) 초대</a>
-        </div>
-        `);
-    });
-    $(support).on("click", () => {
-        let bool = confirm(`${url.support}를 여시겠습니까?`);
-        bool?window.open(url.support):page.html();
-    });
+    })
 });
