06539a59e207a2a713adf52e16237df9fccec7f5
CsepJ <sepbot3240@gmail.com>
2021-08-28 22:12:40 +0900

    GoodBye Sonnet

--- index.js
+++ index.js
@@ -9,12 +9,9 @@ app.set("views", __dirname+"/static");
 app.use(express.static(__dirname+"/static"));
 app.use("/", (req,res) => {
     let path = _url.parse(req.url, true).pathname;
-    if(path == "/" || path == "/index.html" || path == "/index"){
+    if(path == "/" || path == "/index.html" || path == "/index" || path == "/site" || path == "/website" || path == "/page"){
         res.status(200);
         res.render("index.ejs", {tip : tips[Math.floor(Math.random() * tips.length)]});
-    }else if(path == "/invite" || path == "/bots" || path == "/bot" || path == "/add" || path == "/invite.html" || path == "/add.html" || path == "/bots.html" || path == "/bot.html"){
-        res.status(200);
-        res.render("invite.ejs");
     }else if(path == "/sepbot" || path == "/sepbot.html"){
         res.status(200);
         res.redirect("https://discord.com/api/oauth2/authorize?client_id=764104980218118194&permissions=8&scope=bot%20applications.commands");
